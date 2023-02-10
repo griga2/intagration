67f997739662d79065c515a826869f8efd7bf9ea
Reloadlol <elektro.nub@gmail.com>
2023-02-10 17:01:27 +0700

    start make request

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import Axios from Axios;
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async checkConnect() {
+    const url =  'https://testbase/ws/ExchangerRMS?wsdl'
+
+    cosnt headers = {
+      'user-agent': 'sampleTest',
+      'Content-Type': 'text/xml;charset=UTF-8',
+      'soapAction': ''
+      'autorization': 'Base',
+      
+    }
+
+    const xml = 
+    axios({
+      method:'post',
+      url,
+      headers,
+      data:xml,
+    }).then((responce) =>   {
+      const data = parser.parse(response.data, {});
+      console.log(data['s:Envelope']['s:Body'].GetByUIDResponse.GetByUIDResult)
+    }).catch((error) => {
+      console.error(`SOAP FAIL: ${error}`);
+    });
+    
+
+    return 'fuck you';
   }
 }
